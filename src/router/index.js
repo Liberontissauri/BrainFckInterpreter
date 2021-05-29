64de74429561895521d57680a50e2be8dc4b6736
Liberontissauri <jp.liberon@gmail.com>
2021-05-29 13:37:35 +0100

    added route to the interpreter page

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,17 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
 import Home from '../views/Home.vue'
+import Interpreter from '../views/CodePage.vue'
 
 const routes = [
   {
     path: '/',
     name: 'Home',
     component: Home
+  },
+  {
+    path: '/interpreter',
+    name: 'Interpreter',
+    component: Interpreter
   }
 ]
 
