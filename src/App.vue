de33ad71390a0db71e7d55b4d30a365073eedf45
Liberontissauri <jp.liberon@gmail.com>
2021-05-30 16:55:23 +0100

    added transitions to the vue router

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,7 @@
 <template>
-  <router-view/>
+  <transition name="fade" mode="out-in">
+  <router-view> </router-view>
+  </transition>
 </template>
 
 <style>
@@ -7,4 +9,17 @@
     width: 100%;
     min-height: 100%;
   }
+
+  .fade-enter-from,
+  .fade-leave-to {
+    opacity: 0;
+    
+  }
+
+  .fade-enter-active,
+  .fade-leave-active {
+    transition: opacity 2s ease;
+  }
+
+
 </style>
